e1eaff45f767ac7c4c4d082cde61a6658a2ae583
Jose <josejesuscedeno@gmail.com>
2022-02-07 19:49:26 +0000

    probando poner a la derecha

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,9 +2,52 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav class="navbar navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div class="container-fluid">
-				<span class="navbar-brand mb-0 h1">Navbar</span>
+				<a class="navbar-brand" href="#">
+					Navbar
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavDropdown"
+					aria-controls="navbarNavDropdown"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a
+								class="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								Services
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								Contact
+							</a>
+						</li>
+                        <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
