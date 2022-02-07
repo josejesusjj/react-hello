696a7e618a3c3ec7308c710a92ca2ffa174a3363
Jose <josejesuscedeno@gmail.com>
2022-02-07 20:11:05 +0000

    creando container

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 //include images into your bundle
 import Navbar from "./navbar.jsx";
+import Container from "./container.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<Container />
 		</div>
 	);
 };
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,43 +2,45 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
 					Navbar
 				</a>
 				<button
-					class="navbar-toggler"
+					className="navbar-toggler"
 					type="button"
 					data-bs-toggle="collapse"
 					data-bs-target="#navbarNavDropdown"
 					aria-controls="navbarNavDropdown"
 					aria-expanded="false"
 					aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+					<span className="navbar-toggler-icon"></span>
 				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul class="navbar-nav">
-						<li class="nav-item">
+				<div
+					className="collapse navbar-collapse"
+					id="navbarNavDropdown">
+					<ul className="navbar-nav">
+						<li className="nav-item">
 							<a
-								class="nav-link active"
+								className="nav-link active"
 								aria-current="page"
 								href="#">
 								Home
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
 								About
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
 								Services
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
 								Contact
 							</a>
 						</li>
