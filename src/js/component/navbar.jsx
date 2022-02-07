dabafdc6172a3e398ab7bfc9136e821d3e0f4000
Jose <josejesuscedeno@gmail.com>
2022-02-07 19:40:57 +0000

    navbar integrado, comienzo edicion de navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-    return (
-        <h5>Hello world</h5>
-
-    );
-}
+	return (
+		<nav class="navbar navbar-light bg-light">
+			<div class="container-fluid">
+				<span class="navbar-brand mb-0 h1">Navbar</span>
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
