308b6b830ad7cf01ecc06f7d451c2708504f54dc
Jose <josejesuscedeno@gmail.com>
2022-02-07 19:31:26 +0000

    navbar?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import Navbar from "./navbar";
+import Navbar from "./navbar.jsx";
 
 //create your first component
 const Home = () => {
