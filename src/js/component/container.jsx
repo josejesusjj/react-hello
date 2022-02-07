4fb0aea2583a3da42d2dbeb71fa4bfea34c57dc5
Jose <josejesuscedeno@gmail.com>
2022-02-07 20:36:51 +0000

    navbar y jumbotron casi listos, falta cards, footer y dar estilos

--- src/js/component/container.jsx
+++ src/js/component/container.jsx
@@ -1,24 +1,8 @@
 import React from "react";
+import Jumbotron from "./jumbotron.jsx";
 
 const Container = () => {
-	return (
-		<div class="jumbotron">
-			<div class="container">
-				<h1>Lorem ipsum</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
-				<p>
-					<a class="btn btn-primary btn-lg" role="button">
-						Leer más
-					</a>
-				</p>
-			</div>
-		</div>
-	);
+	return <Jumbotron />;
 };
 
 export default Container;
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Jumbotron = () => {
+	return (
+		<div class="jumbotron">
+			<div class="container">
+				<h1>A Warm Welcome!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+					sed do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<p>
+					<a class="btn btn-primary btn-lg" role="button">
+						Call to action!
+					</a>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Jumbotron;
