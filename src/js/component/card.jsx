29222d8f6119a46c119de8278079bf87c1d3a7a7
Jose <josejesuscedeno@gmail.com>
2022-02-09 16:47:09 +0000

    b

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,67 +2,8 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div class="card">
-			<div class="container">
-				<div class="row row-cols-1 row-cols-md-3 g-4">
-					<div class="col">
-						<div class="card h-100">
-							<img src="..." class="card-img-top" alt="..." />
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">
-									This is a wider card with supporting text
-									below as a natural lead-in to additional
-									content. This content is a little bit
-									longer.
-								</p>
-							</div>
-							<div class="card-footer">
-								<small class="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="card h-100">
-							<img src="..." class="card-img-top" alt="..." />
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">
-									This card has supporting text below as a
-									natural lead-in to additional content.
-								</p>
-							</div>
-							<div class="card-footer">
-								<small class="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="card h-100">
-							<img src="..." class="card-img-top" alt="..." />
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">
-									This is a wider card with supporting text
-									below as a natural lead-in to additional
-									content. This card has even longer content
-									than the first to show that equal height
-									action.
-								</p>
-							</div>
-							<div class="card-footer">
-								<small class="text-muted">
-									Last updated 3 mins ago
-								</small>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+		<div>
+			<h1>Hola</h1>
 		</div>
 	);
 };
