23aa46aed239fa10efc4ae58f648898c832f06e1
Jose <josejesuscedeno@gmail.com>
2022-02-09 16:20:00 +0000

    importando card

--- src/js/component/container.jsx
+++ src/js/component/container.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 
 const Container = () => {
 	return <Jumbotron />;
+
 };
 
 export default Container;
+
