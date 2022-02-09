0de230add373fafe3f475192453647a9448a86d8
Jose <josejesuscedeno@gmail.com>
2022-02-09 17:06:31 +0000

    q

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 //include images into your bundle
 import Navbar from "./navbar.jsx";
 import Container from "./container.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
 		<div>
 			<Navbar />
 			<Container />
+			<Footer />
 		</div>
 	);
 };
