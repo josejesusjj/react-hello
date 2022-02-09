63465c9115b97099ba75b31c6bf5689c1f0a47c3
Jose <josejesuscedeno@gmail.com>
2022-02-09 17:00:29 +0000

    n

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -4,18 +4,6 @@ const Card = () => {
 	return (
 		<div class="row row-cols-1 row-cols-md-4 g-4">
 			<div class="col">
-				<div class="card h-100">
-					<img src="..." class="card-img-top" alt="..." />
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">
-							This is a longer card with supporting text below as
-							a natural lead-in to additional content. This
-							content is a little bit longer.
-						</p>
-					</div>
-				</div>
-
 				<div class="card">
 					<img src="..." class="card-img-top" alt="..." />
 					<div class="card-body">
@@ -27,43 +15,63 @@ const Card = () => {
 						</p>
 					</div>
 					<div class="card-footer">
-						<small class="text-muted">
-							Last updated 3 mins ago
-						</small>
+						<a href="#" class="btn btn-primary">
+							Find Out More!
+						</a>
 					</div>
 				</div>
 			</div>
 			<div class="col">
-				<div class="card h-100">
-					<img src="..." class="card-img-top" alt="..." />
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a short card.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card h-100">
+				<div class="card">
 					<img src="..." class="card-img-top" alt="..." />
 					<div class="card-body">
 						<h5 class="card-title">Card title</h5>
 						<p class="card-text">
-							This is a longer card with supporting text below as
-							a natural lead-in to additional content.
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
 						</p>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-primary">
+							Find Out More!
+						</a>
 					</div>
 				</div>
 			</div>
 			<div class="col">
-				<div class="card h-100">
+				<div class="card">
 					<img src="..." class="card-img-top" alt="..." />
 					<div class="card-body">
 						<h5 class="card-title">Card title</h5>
 						<p class="card-text">
-							This is a longer card with supporting text below as
-							a natural lead-in to additional content. This
-							content is a little bit longer.
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
 						</p>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-primary">
+							Find Out More!
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<img src="..." class="card-img-top" alt="..." />
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-primary">
+							Find Out More!
+						</a>
 					</div>
 				</div>
 			</div>
