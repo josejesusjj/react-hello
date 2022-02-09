747e49992e32e10f31dbcfc5c92933598012fbb3
Jose <josejesuscedeno@gmail.com>
2022-02-09 16:23:27 +0000

    no coge el card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
+		<div class="jumbotron">
+			<div class="container">
+				<h1>A Warm Welcome!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+					sed do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<p>
+					<a class="btn btn-primary btn-lg" role="button">
+						Call to action!
+					</a>
+				</p>
+			</div>
 		</div>
 	);
 };
--- src/js/component/container.jsx
+++ src/js/component/container.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Jumbotron from "./jumbotron.jsx";
-import Card from "./card.jsx";
+import Card from ".card.jsx";
 
 const Container = () => {
-	return <Jumbotron />;
-	return <Card />;
+	return (<Jumbotron />), (<Card />);
 };
 
 export default Container;
